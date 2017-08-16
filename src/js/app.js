db7e51fc3256477c059bdf13126c4082899d6515
will <a10000005588@gmail.com>
2017-08-16 14:54:35 +0800

    first version for adoping the pets using the smart contract

---
+++ src/js/app.js
@@ -0,0 +1,122 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    // Load pets.
+    $.getJSON('../pets.json', function(data) {
+      var petsRow = $('#petsRow');
+      var petTemplate = $('#petTemplate');
+
+      for (i = 0; i < data.length; i ++) {
+        petTemplate.find('.panel-title').text(data[i].name);
+        petTemplate.find('img').attr('src', data[i].picture);
+        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-age').text(data[i].age);
+        petTemplate.find('.pet-location').text(data[i].location);
+        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+        petsRow.append(petTemplate.html());
+      }
+    });
+
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Web3 is a JavaScript library for interacting with the Ethereum blockchain.
+
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      // the testrpc you set
+      App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
+  },
+  // we need to instantiate our smart contract so web3 knows where to find it and how it works.
+  initContract: function() {
+
+    $.getJSON('Adoption.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      // It keeps information about our contract in sync with your migrations,
+      // so you don't need to change the contract's deployed address manually.
+
+      // Set the provider for our contract.
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      // Use our contract to retieve and mark the adopted pets.
+      // in case any pets are already adopted from a previous visit.
+      return App.markAdopted();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  },
+
+  handleAdopt: function() {
+    event.preventDefault();
+
+    var petId = parseInt($(event.target).data('id'));
+
+    /*
+     * Replace me...
+     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  markAdopted: function(adopters, account) {
+
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call();
+      //  we then loop through them, checking to see if an address is stored for each pet.
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Pending...').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
